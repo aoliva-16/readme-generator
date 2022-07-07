0b557e058b8e48e48cdaaa21647c73fea8f2e96c
Angelica Olivares <olivares_199@outlook.com>
2022-07-07 10:52:08 -0700

    questions for user input added

--- Develop/index.js
+++ Develop/index.js
@@ -1,7 +1,53 @@
 // TODO: Include packages needed for this application
+const inquirer = require('inquirer')
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'Project tatle?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Project description?'
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'How to install?'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'Project usage?'
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'Contribution info?'
+    },
+    {
+        type: 'input',
+        name: 'email',
+        message: 'For questions(email)?'
+    },
+    {
+        type: 'input', 
+        name: 'github',
+        message: 'For questions(github)?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'License?',
+        choices: ['MIT', 'ISC', 'GNUPLv3'],
+        filter(val) {
+            return val.toLowerCase();
+        }
+    }
+];
 
 // TODO: Create a function to write README file
 function writeToFile(fileName, data) {}
